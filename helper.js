8499c418e7533e8c4edb2d04968cce3a016f8325
mktcode <kontakt@markus-kottlaender.de>
2018-06-08 10:46:12 +0200

    fixed bug in submissions endpoint always return array

--- helper.js
+++ helper.js
@@ -46,27 +46,30 @@ module.exports = {
     });
   },
   getSubmissions(storyPost) {
-    return new Promise((resolve, reject) => {
-      steem.api.getContentReplies(storyPost.author, storyPost.permlink, function(err, comments) {
-        if (err) {
-          reject(err);
-        } else {
-          let submissions = [];
-          comments.forEach(comment => {
-            if (comment.hasOwnProperty('json_metadata') && comment.json_metadata) {
-              let meta = JSON.parse(comment.json_metadata);
-              if (meta.hasOwnProperty('type') && ['append', 'end'].indexOf(meta.type) !== -1) {
-                submissions.push(comment);
+    if (storyPost) {
+      return new Promise((resolve, reject) => {
+        steem.api.getContentReplies(storyPost.author, storyPost.permlink, function(err, comments) {
+          if (err) {
+            reject(err);
+          } else {
+            let submissions = [];
+            comments.forEach(comment => {
+              if (comment.hasOwnProperty('json_metadata') && comment.json_metadata) {
+                let meta = JSON.parse(comment.json_metadata);
+                if (meta.hasOwnProperty('type') && ['append', 'end'].indexOf(meta.type) !== -1) {
+                  submissions.push(comment);
+                }
               }
-            }
-          });
-          submissions.sort((a, b) => {
-            return b.net_votes - a.net_votes;
-          });
-          resolve(submissions);
-        }
+            });
+            submissions.sort((a, b) => {
+              return b.net_votes - a.net_votes;
+            });
+            resolve(submissions);
+          }
+        });
       });
-    });
+    }
+    return [];
   },
   getStories(storyPosts) {
     let stories = [];
